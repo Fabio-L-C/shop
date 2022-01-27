3174b4a2852d62ee29b67add8e76fa4c20a2baa5
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2022-01-27 17:07:10 -0300

    Aula 293 - Carregar Pedidos

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,4 +3,6 @@
 class Constants {
   static const PRODUCT_BASE_URL =
       'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL =
+      'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/orders';
 }
