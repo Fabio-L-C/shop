4908a03614a448586f6c0a9ead0e601825813796
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2022-02-09 19:24:02 -0300

    Aula 312 - Favoritos e Pedidos

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: constant_identifier_names
 
 class Constants {
-  static const PRODUCT_BASE_URL =
-      'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/products';
+  static const USER_FAVORITES_URL =
+      'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/userFavoriteS';
   static const ORDER_BASE_URL =
       'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/orders';
+  static const PRODUCT_BASE_URL =
+      'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/products';
 }
