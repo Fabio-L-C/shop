759a0c59cb24f33b8f05c533cf8ba4de3b69e8d1
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2022-02-11 11:01:07 -0300

    Aula 318 - Autentificação Completa

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,7 @@
 
 class Constants {
   static const USER_FAVORITES_URL =
-      'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/userFavoriteS';
+      'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/userFavorites';
   static const ORDER_BASE_URL =
       'https://shop-cod3r-2dad9-default-rtdb.firebaseio.com/orders';
   static const PRODUCT_BASE_URL =
