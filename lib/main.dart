df3315d94ffaf60a5a875b113e3ba4eba3610c16
Fabio Louzada Colodette <fabio.loutte@gmail.com>
2022-02-16 10:01:15 -0300

    Aula 332 - Projeto Shop Completo

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 
 import 'models/product_list.dart';
 
@@ -60,6 +61,12 @@ class MyApp extends StatelessWidget {
             secondary: Colors.deepOrange,
           ),
           fontFamily: 'Lato',
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.android: CustomPageTransitionsBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionsBuilder(),
+            },
+          ),
         ),
         //home: const ProductsOverviewPage(),
         routes: {
